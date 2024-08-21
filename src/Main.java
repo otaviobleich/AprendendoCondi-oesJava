c3f9f2a39bb5a8fca62b2e094f8bf3acecf81b96
Vinícius Calegari <viniciuscalegari123b5@gmail.com>
2024-08-21 20:52:00 -0300

    Criação do Projeto Java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Integer idadePessoa = 20;
+        String NomePessoa = "Otavio";
+
+
+
+        /*
+        * operadores logicos
+        * > maior
+        * >= Maior Igual
+        * <= Menor Igual
+        * == é igual
+        * != é diferente
+        * equal compara textos
+         */
+
+        if(idadePessoa > 21){
+            System.out.println("é maior de 21");
+        }
+        else{
+            System.out.println("não é maior");
+        }
+
+
     }
 }
